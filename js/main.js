9edc8f5aba73002274560709a965fabae4f81697
shamilmelis <akastramormoley1st@mail.ru>
2023-07-06 11:12:21 +0600

    i made a comments in css like explain a code, and some things

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@ const burgerFunction = document.querySelector('.burger-button');
 burgerFunction.addEventListener('click', function() {
     let container = document.querySelector('.container-responsive-menu');
     container.style.display = "flex";
+    let body = document.querySelector('body');
+    body.classList.add('overflow');
 })
 
 const closeFunction = document.querySelector('.close-burger');
 closeFunction.addEventListener('click', function() {
     let container = document.querySelector('.container-responsive-menu');
     container.style.display = 'none';
+    let body = document.querySelector('body');
+    body.classList.remove('overflow');
 })
